f2ef7d6c2f7dc7a63710ebcd079c1a228829ba63
Tikhomirov Alexandr <al.v.tikhomirov@gmail.com>
2022-08-31 11:57:15 +0300

    switch fix

--- custom_components/ectocontrol/switch.py
+++ custom_components/ectocontrol/switch.py
@@ -15,7 +15,13 @@ import homeassistant.const
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-from homeassistant.components.switch import (SwitchEntity)
+from homeassistant.components.switch import (ToggleEntity)
+from homeassistant.helpers.entity import EntityCategory, ATTR_DEVICE_CLASS
+
+mdis = {
+    21948:{1:"mdi:water-pump",0:"mdi:water-pump-off"},
+    0:{1:"mdi:toggle-switch",0:"mdi:toggle-switch-off"}
+}
 
 class EctoSwitch(SwitchEntity):
     _attr_has_entity_name = True
@@ -32,18 +38,22 @@ class EctoSwitch(SwitchEntity):
 
         self._attr_name = item['config']['name']
         self._attr_unique_id = self._item['id']
+        #self._attr_entity_category = EntityCategory.CONFIG
+        #self._attr_device_class
 
         self._api.setDispatcher(self._item['id'], self.set_state)
 
     def set_state(self, state):
         self._attr_is_on = True if state == 1 else False
+        if self._attr_unique_id in mdis:
+            self._attr_icon = mdis[self._attr_unique_id][state]
+        else:
+            self._attr_icon = mdis[0][state]
 
-    def turn_on(self, **kwargs):
-        """Turn the switch on."""
-        self._api.setState(self._item['id'], "1")
+    async def async_turn_on(self):
+        await self._api.setState(self._item['id'], "1")
         self.set_state(1)
 
-    def turn_off(self, **kwargs):
-        """Turn the switch off."""
-        self._api.setState(self._item['id'], "0")
+    async def async_turn_off(self):
+        await self._api.setState(self._item['id'], "0")
         self.set_state(0)
